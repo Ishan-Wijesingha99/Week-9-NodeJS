39ce1e8acacbd7af6e8e77905e46075467926843
Ishan <ishanwijes@gmail.com>
2022-07-12 20:56:11 +1000

    added files for looping over an object

---
+++ 01-Activities/05-Ins_Let-Const/loopingThroughObjects.js
@@ -0,0 +1,13 @@
+
+const hughObject = {
+    firstName: 'Hugh',
+    lastName: 'Farrell',
+    age: 21,
+    university: 'Monash'
+}
+
+for(const [key, value] of hughObject) {
+    console.log(`${key} --- ${value}`);
+}
+
+
