ed3eab86664896e3cc1d1930034614bf0c2bdc27
Ishan <ishanwijes@gmail.com>
2022-07-12 21:52:44 +1000

    activity 10 completed

--- 01-Activities/10-Stu_Template-Literals/Unsolved/index.js
+++ 01-Activities/10-Stu_Template-Literals/Unsolved/index.js
@@ -1,11 +1,12 @@
 // music should be an object with title, artist, and album properties
 const music = {
-  // code here
- 
+  artist: 'Kanye West',
+  title: 'Gorgeous',
+  album: 'My Beautiful Dark Twisted Fantasy'
 };
 
 // Write code between the backticks tags to output the data from the music object above.
-const songSnippet = ``;
+const songSnippet = `My favourite song by ${music.artist} is ${music.title} from the album ${music.album}`;
 
 console.log(songSnippet)
 
