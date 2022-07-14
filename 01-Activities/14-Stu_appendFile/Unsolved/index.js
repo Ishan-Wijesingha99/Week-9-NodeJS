17ad7fdcf3a673324cbb6cb1b4ff3bef2b8e333e
Ishan <ishanwijes@gmail.com>
2022-07-14 19:26:35 +1000

    completed student activity 14

--- 01-Activities/14-Stu_appendFile/Unsolved/index.js
+++ 01-Activities/14-Stu_appendFile/Unsolved/index.js
@@ -1,8 +1,22 @@
-// TODO: What are we importing?
+// Developers create javascript libraries to make their lives easier. In order to make these libraries available for everyone to use, the register it with services like npm or Yarn.
+
+// .require is a built in function that accepts a string, this string is connected to whatever library is registered with npm/yarn. That way, require() knows exactly what library to import
+
+// this "library" is more often than not an object, and we STORE THIS OBJECT, as a single variable, in this case fs
 const fs = require('fs');
 
-// TODO: Add comments to explain each of the three arguments of appendFile()
+
+
+// because fs is basically an object, it comes with it's own methods. appendFile() is one of those methods that only exist in that "fs" library
+// you can learn more about the .appendFile() method in the documentation
 fs.appendFile('log.txt', `${process.argv[2]}\n`, (err) =>
-  // TODO: Describe how this ternary operator works
+
   err ? console.error(err) : console.log('Commit logged!')
+
 );
+
+
+
+// you can use require() to import files as well
+// could be an array, could be anything
+
