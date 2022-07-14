1b2bcdceb739ccf0ced4021a4f66534aff434846
Ishan <ishanwijes@gmail.com>
2022-07-14 19:38:30 +1000

    student activity 16 completed

--- 01-Activities/16-Stu_maths/Unsolved/index.js
+++ 01-Activities/16-Stu_maths/Unsolved/index.js
@@ -1,6 +1,18 @@
-// TODO: Import `maths.js`
+// TODO: Import `maths.js` so that we can use it's methods and variables in this file
+mathsJS = require('./maths.js');
 
-// TODO: Capture the values passed from the command line into these three variables: `operation`, `numOne` and `numTwo`
+// how cool is this! It's literally just an object. In fact, it's almost identical to the module.exports object defined in maths.js
+console.log(mathsJS);
+
+// This is even cooler! You now have access to methods that were defined in maths.js
+console.log(mathsJS.sum(3, 4));
+console.log(mathsJS.difference(3, 4));
+console.log(mathsJS.product(3, 4));
+console.log(mathsJS.quotient(3, 4));
+
+
+
+
 
 // TODO: Create a `switch` statement that accepts an `operation` parameter
 // and each `case` uses the corresponding `maths` method
--- 01-Activities/16-Stu_maths/Unsolved/maths.js
+++ 01-Activities/16-Stu_maths/Unsolved/maths.js
@@ -1,3 +1,7 @@
+
+// this is how you export functionalities
+// we have a series of math functions here, that we want to access in other files
+// so, exports is just an object that takes methods and variables as properties
 module.exports = {
   sum: (a, b) => a + b,
   difference: (a, b) => a - b,
