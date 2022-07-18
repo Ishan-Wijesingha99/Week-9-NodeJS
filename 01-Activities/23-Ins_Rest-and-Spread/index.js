251d2cf4e280ecb784bbbe05623ce9125582705c
Ishan <ishanwijes@gmail.com>
2022-07-18 19:57:34 +1000

    student activity 24 completed

--- 01-Activities/23-Ins_Rest-and-Spread/index.js
+++ 01-Activities/23-Ins_Rest-and-Spread/index.js
@@ -1,30 +1,50 @@
+
+// the rest operator is usually used INSIDE A FUNCTION PARAMETER
+// if you don't know how many arguments the function will take in, you use the rest operator to take them all and put them into a single array
+
+// spread operator - takes a SINGLE iterable and spreads them into MANY values
+// rest operator - takes MANY values (most of the time an unknown number) and puts them into a SINGLE array
+
+// the examples below really show this
+
+
+
 // without rest
+// if you add more arguments into add() function, javascript won't know what to do
 function add(x, y) {
   return x + y;
 }
-console.log(add(1, 2, 3, 4, 5)); // => 3
 
-// 1. rest
+console.log(add(1, 2, 3, 4, 5)); // => still 3
+
+
+
+// with rest
 function add(...nums) {
   let sum = 0;
   for (let num of nums) sum += num;
   return sum;
 }
 
+// doesn't matter how many arguments you pass in, it will still work
 add(1); // => 1
 add(3, 3); // => 6
 add(1, 1, 4, 5); // => 11
 
-// 2. 
+
+
+// another example 
 function howManyArgs(...args) {
-  return `You passed ${args.length} arguments.`; // point out the template literal
+  return `You passed ${args.length} arguments.`; // its an array, so it has a length
 }
 
 console.log(howManyArgs(0, 1)); // You have passed 2 arguments.
 console.log(howManyArgs("argument!", null, ["one", 2, "three"], 4)); // You have passed 4 arguments.
 
-// 1. spread
-const dragons = ["Drogon", "Viserion", "Rhaegal"];
-const weapons = ["dragonglass", ...dragons, "wildfire"]; // notice the spread operator ...dragons
 
-console.log(weapons); // prints ["dragonglass", "Drogon", "Viserion", "Rhaegal", "wildfire"]
+
+// spread operator
+const dragons = ["Drogon", "Viserion", "Rhaegal"];
+const weapons = ["dragonglass", ...dragons, "wildfire"]; // concatinates the two arrays
+
+console.log(weapons);
--- 01-Activities/24-Stu_Rest-and-Spread/Unsolved/index.js
+++ 01-Activities/24-Stu_Rest-and-Spread/Unsolved/index.js
@@ -1,28 +1,41 @@
 // Exercise 1
 const songs = ['Creep', 'Everlong', 'Bulls On Parade', 'Song 2', 'What I Got'];
 
-// TODO: Which operator is being used here?
+// spread operator is being used to create a new array
 const newSongs = [...songs];
 
-// TODO: What do you expect to be logged in the console?
+// ['Creep', 'Everlong', 'Bulls On Parade', 'Song 2', 'What I Got'] will be logged
 console.log(newSongs);
+
+
 
 // Exercise 2
 const addition = (x, y, z) => {
   const array = [x, y, z];
-  // TODO: What does the reduce() method do?
-  return array.reduce((a, b) => a + b, 0);
+  
+  // reduce loops over the array, and for each iteration, it has access to the previousValue and the currentValue
+  // the purpose of reduce is to take an entire array, and reduce it all into a SINGLE VALUE
+
+  // reduce method takes two parameters.
+  // The first is a function that has two parameters itself, the previousValue, and the currentValue
+  // this function will return whatever value you want, based off previousValue and currentValue
+
+  // at the end of the loop, a single value will be returned
+
+  // you must also specify the initialValue
+  return array.reduce((prevValue, currentValue) => prevValue + currentValue, 0);
 };
-// TODO: What do you expect to be logged in the console?
-console.log(addition(1, 2, 3));
 
-// TODO: What is this syntax that is being used as the parameter?
-const additionSpread = (...array) => {
-  return array.reduce((a, b) => a + b, 0);
+console.log(addition(1, 2, 3)); // 6
+
+
+
+// rest operator is being used so that an array will be created with any number of elements
+// the name of that array will be fullArray
+const additionSpread = (...fullArray) => {
+  return fullArray.reduce((prevValue, currentValue) => prevValue + currentValue, 0);
 };
 
-// TODO: What do you expect to be logged in the console?
-console.log(additionSpread(1, 2, 3));
+console.log(additionSpread(1, 2, 3)); // 6
 
-// TODO: What do you expect to be logged in the console?
-console.log(additionSpread(1, 2, 3, 4, 100));
+console.log(additionSpread(1, 2, 3, 4, 100)); // 110
