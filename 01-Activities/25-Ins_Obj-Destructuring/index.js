b9d2ce8e4b631d40720931d7c47c8fa21e3f307b
Ishan <ishanwijes@gmail.com>
2022-07-18 20:28:08 +1000

    student activity 26 completed

--- 01-Activities/25-Ins_Obj-Destructuring/index.js
+++ 01-Activities/25-Ins_Obj-Destructuring/index.js
@@ -1,53 +1,54 @@
 const arya = {
-  name: 'Arya Stark',
+  aryaName: 'Arya Stark',
   parents: ['Eddard Stark', 'Catelyn Stark'],
 };
 
 const jaime = {
-  name: 'Jaime Lannister',
+  jaimeName: 'Jaime Lannister',
   parents: ['Tywin Lannister', 'Joanna Lannister'],
 };
 
+
+
 // In the past, if we wanted to pull off an object's property we'd have to do something like this:
+const nameArya = arya.aryaName;
+const parentsArya = arya.parents;
 
-const aryaName = arya.name;
-const aryaParents = arya.parents;
+console.log(nameArya); // logs `"Arya Stark"`
+console.log(parentsArya); // logs `["Eddard Stark", "Catelyn Stark"]`
 
-console.log(aryaName); // logs `"Arya Stark"`
-console.log(aryaParents); // logs `["Eddard Stark", "Catelyn Stark"]`
+
 
 // Now with ES6 object destructuring syntax, we can do this:
+const { aryaName, parents } = arya;
 
-const { name, parents } = arya;
+console.log(aryaName); // logs 'Arya Stark'
+console.log(parents); // logs ['Eddard Stark', 'Catelyn Stark']
 
-console.log(name); // logs `"Jaime Lannister"`
-console.log(parents); // logs `["Tywin Lannister", "Joanna Lannister"]`
+
 
 // We can also rename our destructured properties like so:
+const { jaimeName: newNameForJaime } = jaime;
+console.log(newNameForJaime); // logs `"Jaime Lannister"`
 
-const { name: jaimeName } = jaime;
-console.log(jaimeName); // logs `"Jaime Lannister"`
 
-// We can also destructure parameters using the same feature. e.g. previously we might have done something like this:
-
-const logCharacter = (character) =>
-  console.log(
-    `${character.name}'s parents are: ${character.parents[0]} and ${character.parents[1]}.`
-  );
-
-logCharacter(arya);
 
 // We can also use array destructuring to pull off an array's elements
 const locations = ['Winterfell', 'Casterly Rock', 'Kings Landing'];
-const [winterfell, casterlyRock, kingLanding] = locations;
+const [wf, cr, kl] = locations;
 
-console.log(winterfell); // logs `"Winterfell"`
-console.log(casterlyRock); // logs `"Casterly Rock"`
-console.log(kingLanding); // logs `"Kings Landing"`
+console.log(wf); // logs `"Winterfell"`
+console.log(cr); // logs `"Casterly Rock"`
+console.log(kl); // logs `"Kings Landing"`
 
-// But now we can do this:
 
-const betterLogCharacter = ({ name, parents }) =>
-  console.log(`${name}'s parents are: ${parents[0]} and ${parents[1]}.`);
+
+// e.g. previously we might have done something like this:
+const logCharacter = (character) => console.log(`${character.name}'s parents are: ${character.parents[0]} and ${character.parents[1]}.`);
+
+logCharacter(arya);
+
+// now we can do this:
+const betterLogCharacter = ({ name, parents }) => console.log(`${name}'s parents are: ${parents[0]} and ${parents[1]}.`);
 
 betterLogCharacter(jaime);
--- 01-Activities/26-Stu_Obj-Destructuring/Unsolved/index.js
+++ 01-Activities/26-Stu_Obj-Destructuring/Unsolved/index.js
@@ -1,15 +1,18 @@
-// 1. Object
+// Object
 const nodejs = {
   pkgName: 'Node.js',
   type: 'JavaScript runtime environment',
 };
 
-// TODO: Destructure the object 'nodejs'
+// Destructuring the object 'nodejs'
+const {pkgName, type:nodejsType} = nodejs;
 
 console.log(pkgName); // <= Node.js
-console.log(type); // <= JavaScript runtime environment
+console.log(nodejsType); // <= JavaScript runtime environment
 
-// 2. Nested Object
+
+
+// Nested Object
 const js = {
   name: 'JavaScript',
   type: 'programming language',
@@ -26,15 +29,19 @@ const js = {
   },
 };
 
-// TODO: Destructure the nested object 'js'
+// Destructuring the nested object 'js'
+const {framework1, framework2} = js.tools.frameworks;
 
 console.log(framework1); // <= AngularJS
 console.log(framework2); // <= Vue.js
 
-// 3. Arrays
+
+
+// Arrays
 const languages = ['HTML', 'CSS', 'JavaScript'];
 
-// TODO: Destructure the array 'languages'
+// Destructuring the array 
+const [markup, style, scripting] = languages;
 
 console.log(markup, style, scripting); // <= HTML CSS JavaScript
 console.log(markup); // <= HTML
